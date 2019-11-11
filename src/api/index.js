742f6d3553e295dbed9a3370538ec7eb85f9ce2c
liujie <loje2012@gmail.com>
2019-11-11 18:31:35 +0800

    leancloud

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+const baseAPI = 'http://api.zdesigner.cn'
+
+export const API = {
+  baseAPI,
+  // loginAPI: `${baseAPI}/1.1/login`,
+  // userAPI: `${baseAPI}/1.1/users`,
+  // userInfoAPI: `${baseAPI}/1.1/classes/userInfo`,
+  // blogListAPI: `${baseAPI}/1.1/classes/blogList`,
+  // blogsAPI: `${baseAPI}/1.1/classes/blogs`,
+  // collectsAPI: `${baseAPI}/1.1/classes/collects`,
+  // likesAPI: `${baseAPI}/1.1/classes/likes`,
+  // commentsAPI: `${baseAPI}/1.1/classes/comments`
+}
+
+export const APPINFO = {
+  appId: 'vwzM34pywVYeT0lY6G5KlSYM-gzGzoHsz',
+  appKey: '3CTYBXJYIGsSrCGSD11E9p63',
+  masterKey: 'FHrUCgxRNx3bPRieKzDG79rt'
+}
---
+++ src/components/Navtop.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">工具</router-link>
+    <router-link to="/about">活动</router-link>
+    <router-link to="/about">下载</router-link>
+    <router-link to="/about">关于我们</router-link>
+  </div>
+</template>
